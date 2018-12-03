467ab2dd266c2eabbadfb4648953e8ddc2b9817d
roberto14118927 <robertoruiz141189@gmail.com>
2018-12-02 22:26:46 -0600

    tempraturaW

--- incu_arduino/incu.ino
+++ incu_arduino/incu.ino
@@ -3,6 +3,10 @@ const byte interruptPin = 2;
 volatile byte state = LOW;
 int AC_LOAD = 3;   
 int dimming = 60; 
+int ADC1 = 0;
+int ADC2 = 0;
+double LM351 = 0.0;
+double LM352 = 0.0;
 
 void setup() {
   Serial.begin(9600);
@@ -13,6 +17,14 @@ void setup() {
 
 void loop() {
   //digitalWrite(ledPin, state);
+   ADC1 = analogRead(A0); 
+   ADC2 = analogRead(A1);
+   LM351 = ((double)ADC1/1023)*5;
+   LM352 = ((double)ADC2/1023)*5;
+
+   Serial.println(LM351,4); //Imprime la variable double con 4 decimales
+   Serial.println(LM352,4); //Imprime la variable double con 4 decimales
+
    dimming = dimming + 1;
   if (dimming>=60){
     dimming=0;
